8019da378bbe6cb82f97377290b9a21e1433e4a3
Elaine Anderson <eparie@bu.edu>
2018-07-16 12:59:05 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,6 +4,10 @@ class PigLatinizer
 
   def initialize(text)
     @text = text.downcase.split /([aeiou].*)/
+    text = @text.rotate(1)
+    text << "ay"
+    final_text = text.join
+
     binding.pry
   end
 
