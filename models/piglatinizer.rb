f6cbd54f68890349e2c1bbaf206936e60c74a40c
Elaine Anderson <eparie@bu.edu>
2018-07-16 14:20:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,6 @@ class PigLatinizer
     word_array = text.split
     if word_array.count > 1
       word_array.each do |word|
-        binding.pry
         array << piglatinize(word)
         sentence = array.join(" ")
         binding.pry
