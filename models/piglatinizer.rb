b16266bea87234f33aa658499a302281fad0d36b
Elaine Anderson <eparie@bu.edu>
2018-07-16 14:15:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,8 +14,8 @@ class PigLatinizer
       word_array.collect do |word|
         array << piglatinize(word)
         sentence = array.join(" ")
-      end
         binding.pry
+      end
       else
   array = text.split(%r{\s*})
     if array.first == "a" || array.first == "e" || array.first == "i" || array.first == "o" || array.first == "u" || array.first == "A" || array.first == "E" || array.first == "I" || array.first == "O" || array.first == "U"
