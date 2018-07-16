5f4b58700828906925622724a9dc29ac2aec2e1c
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:35:57 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
   def piglatinize(text)
     text = text.split /([aeiou].*)/
     binding.pry
-    if text.first == /([aeiou].*)/
+    if text.first == "a" || text.first == "e" || text.first == "i" || text.first == "o" || text.first == "u"  
       text << "way"
       text.join
     else
