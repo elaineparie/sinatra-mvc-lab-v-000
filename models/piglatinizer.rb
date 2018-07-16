25b975002e38fc06bb40eac705371adcb5a32bcc
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:30:21 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,6 +8,10 @@ class PigLatinizer
 
   def piglatinize(text)
     text = text.split /([aeiou].*)/
+    if text.count == 1
+      text << "way"
+      text.join
+    else 
     text = text.rotate(1)
     text << "ay"
     final_word = text.join
