f12596940aaa290a2e378cd89abe08342ee1d6a5
Elaine Anderson <eparie@bu.edu>
2018-07-16 14:25:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,9 +6,16 @@ class PigLatinizer
     #@text = text.downcase.split /([aeiou].*)/
   end
 
+def sentence_splitter(text)
+  array = []
+  if word_array.count > 1
+    word_array = text.split
+    word_array.each do |word|
+      array << piglatinize(word)
+      sentence = array.join(" ")
+end 
 
   def piglatinize(text)
-      if
   array = text.split(%r{\s*})
     if array.first == "a" || array.first == "e" || array.first == "i" || array.first == "o" || array.first == "u" || array.first == "A" || array.first == "E" || array.first == "I" || array.first == "O" || array.first == "U"
       array << "way"
