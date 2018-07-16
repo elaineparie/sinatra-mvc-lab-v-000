9090439cef99e3f651f41f4c91a2f4272d5b835a
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:08:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,13 @@
 require 'pry'
 class PigLatinizer
-  attr_reader :words
+  attr_reader :text
 
-  def initialize(words)
-    @words = word.downcase.split /([aeiou].*)/
+  def initialize(text)
+    @text = text.downcase.split /([aeiou].*)/
   end
 
   def piglatin
-    word = @words.rotate(1)
+    word = @text.rotate(1)
     word << "ay"
     final_word = text.join
     final_word
