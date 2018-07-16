253c232115ee62833fc915ac2c87512ca8fd7a3f
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:58:05 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,11 +6,15 @@ class PigLatinizer
     #@text = text.downcase.split /([aeiou].*)/
   end
 
-  def piglatinize(text)
+  def how_many_words(text)
     word_array = text.split
     if word_array.count > 1
+      word1 = piglatinize(word_array[0])
+      word2 = piglatinize(word_array[1])
+      word3 = piglatinize(word_array[2])
+  end
 
-    binding.pry
+  def piglatinize(text)
   array = text.split(%r{\s*})
     if array.first == "a" || array.first == "e" || array.first == "i" || array.first == "o" || array.first == "u" || array.first == "A" || array.first == "E" || array.first == "I" || array.first == "O" || array.first == "U"
       array << "way"
