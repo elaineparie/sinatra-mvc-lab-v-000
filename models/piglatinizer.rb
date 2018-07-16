2fdbf10702c435af9d0d550820d0545109af3304
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:58:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,6 +14,7 @@ class PigLatinizer
       word3 = piglatinize(word_array[2])
   end
 end
+
   def piglatinize(text)
   array = text.split(%r{\s*})
     if array.first == "a" || array.first == "e" || array.first == "i" || array.first == "o" || array.first == "u" || array.first == "A" || array.first == "E" || array.first == "I" || array.first == "O" || array.first == "U"
