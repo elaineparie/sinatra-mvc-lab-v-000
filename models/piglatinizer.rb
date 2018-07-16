d559ed536ae3692c9e8b951e23cdac19d75be0fd
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:46:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,6 @@ class PigLatinizer
 
   def piglatinize(text)
   array = text.split(%r{\s*})
-  binding.pry
     if array.first == "a" || array.first == "e" || array.first == "i" || array.first == "o" || array.first == "u" || array.first == "A" || array.first == "E" || array.first == "I" || array.first == "O" || array.first == "U"
       array << "way"
       array.join
