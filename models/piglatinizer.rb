e799fd724f8df1fac24327f165241c46d33d8840
Elaine Anderson <eparie@bu.edu>
2018-07-16 13:22:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,11 +7,17 @@ class PigLatinizer
   end
 
   def piglatinize(text)
+    if text.count == 1
+    array = @text.split
+    array << "way"
+    array.join
+  else
     @text = text.split /([aeiou].*)/
     text = @text.rotate(1)
     text << "ay"
     final_word = text.join
     final_word
+
   end
 
 end
